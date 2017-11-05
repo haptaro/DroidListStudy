4291503c071d00447206779f6cd7400766d1430f
Kotaro Fukuo <fukuo@sansan.com>
2017-11-06 08:33:52 +0900

    ListViewを追加してmyListViewとidを付ける

--- app/src/main/kotlin/com/fukuosansan/powerappsactivity/MainActivity.kt
+++ app/src/main/kotlin/com/fukuosansan/powerappsactivity/MainActivity.kt
@@ -2,11 +2,15 @@ package com.fukuosansan.powerappsactivity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val myListView = findViewById<ListView>(R.id.myListView)
+        
     }
 }
