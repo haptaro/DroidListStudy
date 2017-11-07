ba535e29132ee9f9a4158df2f49afc202fbc5e1f
Kotaro Fukuo <fukuo@sansan.com>
2017-11-07 09:04:00 +0900

    ListViewにアイテムを表示できるようにした

--- app/src/main/kotlin/com/fukuosansan/powerappsactivity/MainActivity.kt
+++ app/src/main/kotlin/com/fukuosansan/powerappsactivity/MainActivity.kt
@@ -1,16 +1,37 @@
 package com.fukuosansan.powerappsactivity
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val myListView = findViewById<ListView>(R.id.myListView)
-        
+
+        val myListViewAdapter = MyListViewAdapter(this)
+
+        myListViewAdapter.add("あいうえお")
+        myListViewAdapter.add("かきくけこ")
+        myListViewAdapter.add("さしすせそ")
+        myListViewAdapter.add("たちつてと")
+        myListViewAdapter.add("なにぬねの")
+        myListViewAdapter.add("はひふへほ")
+        myListViewAdapter.add("まみむめも")
+        myListViewAdapter.add("やいゆえよ")
+
+        myListView.adapter = myListViewAdapter
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        println("=====: アイテムがクリックされました!!!")
+        val intent = Intent(this, DetailActivity::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/kotlin/com/fukuosansan/powerappsactivity/MyListViewAdapter.kt
@@ -0,0 +1,29 @@
+package com.fukuosansan.powerappsactivity
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+
+/**
+ * Created by fukuo on 2017/11/07.
+ */
+class MyListViewAdapter(context: Context): ArrayAdapter<String>(context, 0) {
+    // あとでカスタムViewを取ってくるので、レイアウトインフレイターを持たせておく
+    private val layoutInflater = LayoutInflater.from(context)
+
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        var contentView = convertView
+
+        if(contentView == null) {
+            contentView = layoutInflater.inflate(R.layout.my_listview_item, parent, false)
+        }
+
+        contentView?.findViewById<TextView>(R.id.eventId)?.text = getItem(position)
+
+        return contentView!!
+    }
+}
